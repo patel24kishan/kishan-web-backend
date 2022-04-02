b147b1fb81f318baf4499b9245762db2e25a7e1c
Kishan <ks805556@dal.ca>
2022-04-02 14:47:01 -0300

    fetching all entries

--- routes/search.js
+++ routes/search.js
@@ -3,8 +3,15 @@
 const router = require("express").Router();
 let Accomodation = require("../models/accomodation.model");
 
-router.route("/").get((req, res) => {
-  res.send("Here are the accomodations");
+router.route("/").get(async(req, res) => {
+//   res.send("simpleSearch");
+  try{
+        const accomodationList =  await Accomodation.find()
+        return res.status(500).json({success:"true",message:"Users Retrieved",data:accomodationList});
+
+    }catch(err){
+        return res.status(500).json({success:"false",message:"Users Not Retrieved"});
+    }
 });
 
 module.exports = router;
