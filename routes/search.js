4c8b9545fe682fe4112f0addf41be253bdde35c0
Kishan <ks805556@dal.ca>
2022-04-02 12:33:41 -0300

    change to search.js

--- routes/search.js
+++ routes/search.js
@@ -3,12 +3,10 @@
 const router = require("express").Router();
 let Accomodation = require("../models/accomodation.model");
 
-router.route("/").get((req, res) => {
+router.route("/search").get((req, res) => {
   res.send("Here are the accomodations");
 });
 
-router.route("/search/:target").get((req, res) => {
- 
-});
+
 
 module.exports = router;
